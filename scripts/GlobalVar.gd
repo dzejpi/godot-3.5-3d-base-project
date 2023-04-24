ae55e0eecef825daa3f9537aeb2c1be0c37da40c
DzejPi <dzejjpii@gmail.com>
2023-04-24 19:00:30 +0200

    Added functions for playing/stopping music and sounds into the global variable.

--- scripts/GlobalVar.gd
+++ scripts/GlobalVar.gd
@@ -6,10 +6,31 @@ onready var music_node = $MusicPlayer
 
 var is_game_over = false
 
+# Necessary to replace null with a proper preload("res://...")
+var music_game_music = null
 
-func _ready():
-	initialise_variables()
+# Necessary to replace null with a proper preload("res://...")
+var sfx_sound_placeholder = null
 
 
-func initialise_variables():
-	pass
+func play_music():
+	music_node.stream = music_game_music
+	music_node.play()
+	
+
+func stop_music():
+	music_node.stop()
+
+
+func play_sound(sfx_name):
+	match(sfx_name):
+		"placeholder":
+			sfx_node.stream = sfx_sound_placeholder
+			sfx_node.play()
+
+
+func stop_sound(sfx_name):
+	match(sfx_name):
+		"placeholder":
+			sfx_node.stream = sfx_sound_placeholder
+			sfx_node.stop()
